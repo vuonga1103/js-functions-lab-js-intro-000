02061260e63e023a51275d6ae642feb5558bb6d6
Anh Vuong <vuonga1103@yahoo.com>
2019-12-16 01:48:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(days, holiday) {
   var days = 20;
-  var holiday = 'Mother's Day'
+  var holiday = 'Mother's Day';
     return `It's ${days} days until ${holiday}!`
 };
